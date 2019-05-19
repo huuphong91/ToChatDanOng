b1dca6cc4d5d8a7ee546c84efdb428951cca434b
Mr.Huu <huuphong91@gmail.com>
2019-05-19 21:43:31 +0700

    imp. project structure mvp + dagger

--- app/src/main/java/vip/mmoney/tochatdanong/di/ActivityBuilderModule.kt
+++ app/src/main/java/vip/mmoney/tochatdanong/di/ActivityBuilderModule.kt
@@ -2,12 +2,23 @@ package vip.mmoney.tochatdanong.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import vip.mmoney.tochatdanong.MainActivity
+import vip.mmoney.tochatdanong.ChapterContentActivity
+import vip.mmoney.tochatdanong.main.MainActivity
+import vip.mmoney.tochatdanong.SplashScreenActivity
+import vip.mmoney.tochatdanong.main.MainModule
 
 @Module
 abstract class ActivityBuilderModule {
 
     @ActivityScope
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [MainModule::class])
     internal abstract fun contributeMainActivity(): MainActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector
+    internal abstract fun contributeSplashScreenActivity(): SplashScreenActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector
+    internal abstract fun contributeChapterContentActivity(): ChapterContentActivity
 }
---
+++ app/src/main/java/vip/mmoney/tochatdanong/services/DataService.kt
@@ -0,0 +1,17 @@
+package vip.mmoney.tochatdanong.services
+
+import vip.mmoney.tochatdanong.model.Chapter
+
+object DataService {
+
+    val chapters = listOf(
+        Chapter("LỜI NÓI ĐẦU", "0.htm"),
+        Chapter("Tố Chất 1 - KIỂM SOÁT TÂM TRẠNG", "1.htm"),
+        Chapter("Tố Chất 2 - CHỊU KHÓ HỌC HỎI", "2.htm"),
+        Chapter("Tố Chất 3 - PHÁT HUY SÁNG TẠO", "3.htm"),
+        Chapter("Tố Chất 4 - TỰ TIẾP THỊ CHO MÌNH", "4.htm"),
+        Chapter("Tố Chất 5 - GIỎI GIAO TIẾP", "5.htm"),
+        Chapter("Tố Chất 6 - NẮM BẮT CƠ HỘI", "6.htm"),
+        Chapter("Tố Chất 7 - HÀNH ĐỘNG", "7.htm"),
+        Chapter("Tố Chất 8 - TÀI TỔ CHỨC LÃNH ĐẠO", "8.htm"))
+}
