e8141f30b16ec6ec5640b0da6a5924c11836a42a
Huu Phong <huuphong91@gmail.com>
2019-05-26 13:50:00 +0700

    add images, imp firebase

--- app/src/main/java/vip/mmoney/tochatdanong/utilities/Constants.kt
+++ app/src/main/java/vip/mmoney/tochatdanong/utilities/Constants.kt
@@ -1,4 +1,4 @@
 package vip.mmoney.tochatdanong.utilities
 
-const val APP_ID = "ca-app-pub-3940256099942544~3347511713"
-const val INTERSTITIAL_AD_ID = "ca-app-pub-3940256099942544/1033173712"
+const val APP_ID = "ca-app-pub-7147375377527505~3271105648"
+const val INTERSTITIAL_AD_ID = "ca-app-pub-7147375377527505/1743892249"
